03a4adac2a5fb650ad817391a4927740898a15e3
BrittonHelfert <brittonhelfert@gmail.com>
2026-05-06 19:22:17 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -15,7 +15,10 @@ def decode_bencode(bencoded_value):
         if first_colon_index == -1:
             raise ValueError("Invalid encoded value")
         return bencoded_value[first_colon_index + 1 :]
-    elif chr(bencoded_value[0]) == "l" and chr(bencoded_value[-1]) == "e":
+
+    print(chr(bencoded_value[0]))
+    print(chr(bencoded_value[-1]))
+    if chr(bencoded_value[0]) == "l" and chr(bencoded_value[-1]) == "e":
         return bencoded_value[1:-1]
     else:
         raise NotImplementedError("Not supported")
