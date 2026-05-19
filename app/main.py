ef8e11084bfd21e3c070be7bc85e48df68e5e1fb
BrittonHelfert <brittonhelfert@gmail.com>
2026-05-19 15:04:59 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import json
 import sys
+from quopri import decode
+from typing import Any, List, Tuple
 
 # import bencodepy - available if you need it!
 # import requests - available if you need it!
@@ -18,24 +20,25 @@ def decode_bencode(bencoded_value):
     elif chr(bencoded_value[0]) == "i" and chr(bencoded_value[-1]) == "e":
         # return without quotations
         return int(bencoded_value[1:-1])
-    elif chr(bencoded_value[0]) == "l":
-        return decode_bencode_list(bencoded_value[1:])
+    elif chr(bencoded_value[0]) == "l" and chr(bencoded_value[-1]) == "e":
+        decoded_list, _ = decode_bencode_list(bencoded_value[1:])
+        return decoded_list
     else:
         raise ValueError("Invalid encoded value")
 
 
-def decode_bencode_list(bencoded_value):
-    unparsed = bencoded_value
+def decode_bencode_list(unparsed_bytes: bytes) -> Tuple[List[Any], int]:
+    unparsed = unparsed_bytes
+    bytes_consumed = 0
     res = []
     while unparsed:
         if len(unparsed) == 0:
             break
         if chr(unparsed[0]) == "l":
-            l = decode_bencode_list(unparsed[1:])
-            res.append(l)
-            character_length_of_l = sum([len(str(item)) for item in l])
-            unparsed = unparsed[1 + character_length_of_l :]
-
+            decoded_list, bytes_consumed_l = decode_bencode_list(unparsed[1:])
+            res.append(decoded_list)
+            bytes_consumed += 1 + bytes_consumed_l
+            unparsed = unparsed[1 + bytes_consumed_l :]
         if chr(unparsed[0]) == "e":
             break
         if chr(unparsed[0]).isdigit():
@@ -46,15 +49,17 @@ def decode_bencode_list(bencoded_value):
             unparsed = unparsed[first_colon_index + 1 :]
             res.append(unparsed[:length])
             unparsed = unparsed[length:]
+            bytes_consumed += first_colon_index + 1 + length
         elif chr(unparsed[0]) == "i":
             first_e_index = unparsed.find(b"e")
             if first_e_index == -1:
                 raise ValueError("Invalid encoded value")
             res.append(int(unparsed[1:first_e_index]))
             unparsed = unparsed[first_e_index + 1 :]
+            bytes_consumed += first_e_index + 1
         else:
             break
-    return res
+    return (res, bytes_consumed)
 
 
 def main():
