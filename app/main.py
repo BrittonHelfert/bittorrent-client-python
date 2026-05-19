ab6916308c1ace4e7e1bf26a33899da48bb1b91e
BrittonHelfert <brittonhelfert@gmail.com>
2026-05-19 15:54:25 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import sys
 from typing import Any, List, Tuple
@@ -117,13 +118,51 @@ def decode_bencode_value(unparsed_bytes: bytes) -> Tuple[Any, int]:
         raise ValueError(f"Invalid encoded value: {unparsed_bytes}")
 
 
+def bencode_dict(dict: dict) -> bytes:
+    result = b"d"
+    for key, value in sorted(dict.items()):
+        result += bencode_string(key) + bencode_arbitrary(value)
+    result += b"e"
+    return result
+
+
+def bencode_string(string: bytes) -> bytes:
+    return f"{len(string)}:{string}".encode()
+
+
+def bencode_int(number: int) -> bytes:
+    return f"i{number}e".encode()
+
+
+def bencode_list(list: list) -> bytes:
+    result = b"l"
+    for item in list:
+        result += bencode_arbitrary(item)
+    result += b"e"
+    return result
+
+
+def bencode_arbitrary(value) -> bytes:
+    if isinstance(value, bytes):
+        return bencode_string(value)
+    if isinstance(value, int):
+        return bencode_int(value)
+    if isinstance(value, list):
+        return bencode_list(value)
+    if isinstance(value, dict):
+        return bencode_dict(value)
+    raise ValueError(f"Unsupported type: {type(value)}")
+
+
 def parse_file_info(bencoded_value: bytes):
     if chr(bencoded_value[0]) != "d":
         raise ValueError("Invalid torrent file: must be a dictionary")
     try:
         decoded_dict, _ = decode_bencode_dict(bencoded_value[1:])
+        encoded_info = bencode_dict(decoded_dict[b"info"])
         print(f"Tracker URL: {decoded_dict[b'announce'].decode()}")
         print(f"Length: {decoded_dict[b'info'][b'length']}")
+        print(f"Info hash: {hashlib.sha1(encoded_info).hexdigest()}")
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
 
